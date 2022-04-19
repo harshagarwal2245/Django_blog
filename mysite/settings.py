a9613a9f882e054be5a63388cf69d5fa33997d37
Harsh Agarwal <agarwalharsh244@gmail.com>
2022-04-19 10:20:18 +0530

    Used heroku db

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import os
 from decouple import config
 import django_heroku
+import dj_database_url
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -101,6 +104,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
